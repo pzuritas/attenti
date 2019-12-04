e56c1a00d652d6cd33c8456cbb6179d18642558c
Pablo Zurita <pzurita@uc.cl>
2019-12-04 13:13:26 -0300

    avances de trabajo

--- data_processor.py
+++ data_processor.py
@@ -1,29 +1,46 @@
 import serial
 import time as tm
 from collections import namedtuple
+import matplotlib.pyplot as plt
 
-time = tm.time()
+zero_time = tm.time()
 session_data = dict()
 Datapoint = namedtuple('data', ['time', 'voltage', 'temperature'])
 
-with serial.Serial(port=3, baudrate=9600, timeout=5) as port0:
-        while True:
+with serial.Serial(port='COM4', baudrate=9600, timeout=5) as port0:
+        for _ in range(200):
             print('In while')
-            data_0 = port0.readline()
+            data_0 = port0.readline().strip()
             print('data_0 read: ', data_0)
-            data_1 = port0.readline()
+            data_1 = port0.readline().strip()
             print('data_1 read: ', data_1)
             if data_0[0] == 'V':
                 print('V')
-                voltage = float(data_0[2:])
-                temperature = float(data_1[2:])
-            else:
-                print('Not V')
                 voltage = float(data_1[2:])
                 temperature = float(data_0[2:])
+            else:
+                print('Not V')
+                voltage = float(data_0[2:])
+                temperature = float(data_1[2:])
             print('data recognized')
-            time = tm.time() - time
+            time = tm.time() - zero_time
             session_data[time] = Datapoint(time, voltage, temperature)
-            tm.sleep(0.25)
-            print('.')
+            print(session_data[time])
+            tm.sleep(0.15)
+            print('-'*15)
             print(len(session_data))
+
+voltage = list(map(lambda dp: dp.voltage, session_data.values()))
+temp = list(map(lambda dp: dp.temperature, session_data.values()))
+
+plt.figure(figsize=(8,4))
+plt.subplot(121)
+plt.plot(list(session_data.keys()), voltage)
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Voltaje (V)')
+plt.subplot(122)
+plt.plot(list(session_data.keys()), temp)
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Temperatura (K)')
+plt.suptitle('Variables en una sesión')
+plt.show()
--- measurment/measurment.ino
+++ measurment/measurment.ino
@@ -1,7 +1,8 @@
 
 char voltmeter_pin = 'A0';
 int temperature_pin = 'A7';
-int offset = 20; //correction value, may be changed
+int offset = 110; //correction value, may be changed
+float temp_offset = -54.89;
 
 void setup()
 {
@@ -10,17 +11,19 @@ void setup()
 
 void loop()
 {
-    int voltage = analogRead(voltmeter_pin);
-    int temperature = analogRead(temperature_pin);
+    float voltage = analogRead(voltmeter_pin);
+    voltage -= offset;
+    float temperature = analogRead(temperature_pin);
     float kelvin_reading = log(10000.0 * ((1024.0 / temperature - 1)));
     kelvin_reading = 1 / (
         0.001129148 + (
             0.000234125 + (0.0000000876741 * pow(kelvin_reading, 2))
             ) * kelvin_reading
         );
+    kelvin_reading -= temp_offset;
     Serial.print("V,");
     Serial.println(voltage);
     Serial.print("T,");
     Serial.println(kelvin_reading);
-    delay(250);
+    delay(150);
 }
