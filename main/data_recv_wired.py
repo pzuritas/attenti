7a2edbb507e5ea7a9d9fad24bacab85c48577a3d
pzuritas <pzurita@uc.cl>
2020-07-01 02:00:56 -0400

    wrapper class done

--- main/data_recv_wired.py
+++ main/data_recv_wired.py
@@ -1,14 +1,18 @@
 import serial
 import time as tm
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.style.use('ggplot')
 from collections import namedtuple
-from pc_parameters import PORT, BAUDRATE, TIMEOUT
+from pc_parameters import PORT, BAUDRATE, TIMEOUT, PERIOD
 
 
 # Define a simple data structure for datapoints.
 Datapoint = namedtuple('data', ['time', 'voltage', 'temperature'])
+SessionData = namedtuple('session', ['voltage', 'temperature'])
 
 class DataReceiver():
-    '''Wrapper class for data receiving.'''
+    '''Wrapper class for data receiving through serial port.'''
     def __init__(self):
         self.port = PORT
         self.baudrate = BAUDRATE
@@ -46,5 +50,50 @@ class DataReceiver():
                 self.session_data[time] = Datapoint(time, voltage, temperature)
                 tm.sleep(self.period)
 
-voltage = list(map(lambda dp: dp.voltage, session_data.values()))
-temp = list(map(lambda dp: dp.temperature, session_data.values()))
+    def save_capture(self):
+        '''Saves data on last session.'''
+        voltage = list(map(lambda dp: dp.voltage, self.session_data.values()))
+        temp = list(map(lambda dp: dp.temperature, self.session_data.values()))
+        time = self.session_data.keys()
+        volt_series = zip(time, voltage)
+        temp_series = zip(time, temp)
+        self.all_data.append(SessionData(volt_series, temp_series))
+
+    def plot_session(self, filename, i=0, show=False):
+        '''Plots the i-th session'''
+        plt.figure()
+        plt.title(f'session {i}')
+        plt.subplot(1, 2, 1)
+        plt.xlabel('time [s]')
+        plt.ylabel('voltage [mV]')
+        plt.plot(self.all_data[i]['voltage'])
+        plt.subplot(1, 2, 2)
+        plt.xlabel('time [s]')
+        plt.ylabel('temperature [K]')
+        plt.plot(self.all_data[i]['temperature'])
+        if show:
+            plt.show()
+        plt.savefig(f'figures/{filename}.png')
+
+    def plot_volt(self, filename, i=0, show=False):
+        '''Plots the i-th voltage series'''
+        plt.figure()
+        plt.title(f'voltage series for session {i}')
+        plt.xlabel('time [s]')
+        plt.ylabel('voltage [mV]')
+        plt.plot(self.all_data[i]['voltage'])
+        if show:
+            plt.show()
+        plt.savefig(f'figures/{filename}.png')
+
+    def plot_temp(self, filename, i=0, show=False):
+        '''Plots the i-th temperature series'''
+        plt.figure()
+        plt.title(f'voltage series for session {i}')
+        plt.xlabel('time [s]')
+        plt.ylabel('temperature [K]')
+        plt.plot(self.all_data[i]['temperature'])
+        if show:
+            plt.show()
+        plt.savefig(f'figures/{filename}.png')
+
