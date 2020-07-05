5fab9b7ec34e88c5ff731f44e0f33ff07c668bdb
clescobar <67849321+clescobar@users.noreply.github.com>
2020-07-05 00:09:21 -0400

    Add files via upload

---
+++ clescobar/measurment.ino
@@ -0,0 +1,29 @@
+
+int voltmeter_pin = A2; 
+int temperature_pin = A7;
+int offset = 750; //correction value, may be changed
+float temp_offset = 47.425;
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    float voltage = analogRead(voltmeter_pin);
+    voltage -= offset;
+    float temperature = analogRead(temperature_pin);
+    float kelvin_reading = log(10000.0 * ((1024.0 / temperature - 1)));
+    kelvin_reading = 1 / (
+        0.001129148 + (
+            0.000234125 + (0.0000000876741 * pow(kelvin_reading, 2))
+            ) * kelvin_reading
+        );
+    kelvin_reading -= temp_offset;
+    Serial.print("V,");
+    Serial.println(voltage);
+    Serial.print("T,");
+    Serial.println(kelvin_reading);
+    delay(833);
+}
