71c46e3a15f1048a6ed07f3cb9479843255a7745
pzuritas <pzurita@uc.cl>
2020-07-05 19:41:14 -0400

    added data receiver for wifi class

--- main/data_recv_wireless.py
+++ main/data_recv_wireless.py
@@ -6,6 +6,7 @@ from data_recv_wired import DataReceiver
 UDPReceived = namedtuple('data', ['host', 'port', 'data'])
 
 class Server():
+    '''Class to start a server for wireless connection.'''
     def __init__(self):
         self.data_recv = defaultdict(list)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,6 +15,9 @@ class Server():
         self.end = False
 
     def run(self):
+        '''
+        Starts the server for data collection. Must be runn after starting datastream.
+        '''
         while not self.end:
             data, (client_host, client_port) = self.socket.recvfrom(4096)
             self.data_recv[client_host] = UDPReceived(
@@ -27,4 +31,20 @@ class Server():
         self.counter = 0
 
 class WiFiDataReceiver(DataReceiver):
-    pass
+    '''Overloads the DataReceiver methods to work with wifi.'''
+    def __init__(self):
+        super().__init__()
+        self.server = Server()
+    
+    def capture_start(self, session_length):
+        self.set_time()
+        self.server.run()
+        data_list = self.server.data_recv.values()
+        for data_tuple in data_list:
+            volt, temp = data_tuple['data'].strip().split(';')
+            volt = float(volt)
+            temp = float(temp)
+            time = tm.time() - self.t0
+            # Data is saved as dictionary to ensure no repeat information
+            self.session_data[time] = Datapoint(time, volt, temp)
+
