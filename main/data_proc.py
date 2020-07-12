684c3561c7f89a57849769080dd40e0b7a34b455
pzuritas <pzurita@uc.cl>
2020-07-12 13:02:14 -0400

    final data processing caps

--- main/data_proc.py
+++ main/data_proc.py
@@ -8,7 +8,8 @@ class DataProcessor():
     def __init__(self):
         self.time = []
         self.data = []
-        self.avg_att = 0
+        self.avg_att_v = 0
+        self.avg_att_t = 0
     
     def read_csv(self, filename):
         '''Read voltage or temperature file.'''
@@ -23,13 +24,39 @@ class DataProcessor():
         '''Detects peaks for data analysis.'''
         if len(self.time) == 0:
             raise ValueError('No data has been loaded.')
-        indices = sp.signal.find_peaks(self.data, threshold)
+        indices = sp.signal.find_peaks(-1*self.data, threshhold)
         timestamps = [self.time[i] for i in indices]
-        self.avg_att = np.mean(np.diff(timestamps))
+        self.avg_att_v = np.mean(np.diff(timestamps))
+
+    def detect_lowering(self, tolerance):
+        '''Detects lowering of signal for data analysis.'''
+        if len(self.time) == 0:
+            raise ValueError('No data has been loaded.')
+        lengths = []
+        current_length = 0
+        for index in range(len(self.data)):
+            if index == 0:
+                pass
+            else:
+                if abs(self.data[index-1] - self.data[index]) < tolerance:
+                    current_length += 1
+                else:
+                    lengths.append(current_length)
+                    current_length = 0
+        index_attention_mean = round(np.mean(lengths))
+        try:
+            self.avg_att_t = self.time[index_attention_mean]
+        except:
+            raise ValueError('Corrupted temperature data.')
 
     def show_avg(self):
         print(
             f'The avergate attention span in the session loaded is'\
-            + f'approximately {round(self.avg_att)} seconds, or'\
-            + f'{round(self.avg_att / 60)} minutes.'
+            + f'approximately {round(self.avg_att_v)} (voltage) seconds, or'\
+            + f'{round(self.avg_att_v / 60)} minutes.'
+            )
+        print(
+            f'The avergate attention span in the session loaded is'\
+            + f'approximately {round(self.avg_att_t)} (temperature) seconds,'\
+            + f'or {round(self.avg_att_t / 60)} minutes.'
             )
--- main/pc_parameters.py
+++ main/pc_parameters.py
@@ -5,3 +5,4 @@ BAUDRATE=9600
 TIMEOUT=5
 PERIOD=0.15 # Period for data receiving. Must be lower than controller period.
 MACPORT='/dev/tty.usbserial-14630'
+FREQ=15
