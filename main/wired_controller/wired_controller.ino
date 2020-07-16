12be193e564c8a45fdf33050326de8c2c6f4b628
claudioescobar <clescobar@uc.cl>
2020-07-16 18:37:09 -0400

    ajuste de codigo

--- main/wired_controller/wired_controller.ino
+++ main/wired_controller/wired_controller.ino
@@ -3,7 +3,7 @@
 const int GSR=A2;
 int sensorValue=0;
 int gsr_average=0;
-
+int temp_average = 0;
 // PARAMETERS: CHANGE ACCORDING TO TEST
 //int voltmeter_pin = 0; // Voltage/CSR pin, parameter
 int ThermistorPin = 7; // Thermistor pin
@@ -30,8 +30,8 @@ void loop()
     //float voltage = analogRead(voltmeter_pin); // Volage measurment
     //voltage -= offset;
     long sumV=0;
-    long sumT=0
-  for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
+    long sumT=0;
+    for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
       {
       float voltage = analogRead(GSR);
       sumV += voltage;
@@ -41,12 +41,12 @@ void loop()
       temp = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
       temp = temp - 273.15;
       temp -= temp_offset;
-      sumT += temp
+      sumT += temp;
       }
-    gsr_average = sumV/10
-    temp_average = sumT/10
+    gsr_average = sumV/10;
+    temp_average = sumT/10;
     Serial.print("V,");
-    Serial.println(grs_average);
+    Serial.println(gsr_average);
     Serial.print("T,");
     Serial.println(temp_average);
     delay(period);
